f1254536e6df073c266be203a8dc712ecf0a9710
Mr Paul <124022001+Dimswati@users.noreply.github.com>
2023-11-05 17:54:53 +0000

    swiper direction

--- components/app/HomeSlider/SwiperCards.tsx
+++ components/app/HomeSlider/SwiperCards.tsx
@@ -45,7 +45,7 @@ const SwiperCards = (props: Props) => {
         return width <= 768 ? "horizontal" : "vertical"
     }
 
-    if(!isClient) {
+    if (!isClient) {
         return null
     }
 
@@ -55,7 +55,8 @@ const SwiperCards = (props: Props) => {
                 modules={[EffectFade, Autoplay, Controller]}
                 autoplay={{
                     delay: 5000,
-                    disableOnInteraction: false
+                    disableOnInteraction: false,
+                    waitForTransition: false
                 }}
                 loop
                 spaceBetween={0}
@@ -124,7 +125,7 @@ const SwiperCards = (props: Props) => {
                             <Link href="/blog/blog-one" className='bg-gradient-to-r from-white 
                 to-white bg-[length:0px_1px]
                 hover:bg-[length:100%_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 cursor-pointer'>
-                               Spicy Crispy Chicken Burger Recipe
+                                Spicy Crispy Chicken Burger Recipe
                             </Link>
                         </h2>
                         <div className='flex gap-x-2 items-center'>
@@ -147,7 +148,99 @@ const SwiperCards = (props: Props) => {
                             <Link href="/blog/blog-one" className='bg-gradient-to-r from-white 
                 to-white bg-[length:0px_1px]
                 hover:bg-[length:100%_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 cursor-pointer'>
-                               Will Humans Be Able to Live in Mars in the Future?
+                                Will Humans Be Able to Live in Mars in the Future?
+                            </Link>
+                        </h2>
+                        <div className='flex gap-x-2 items-center'>
+                            <Image src={author} alt='author' className='rounded-full' />
+                            <div className='uppercase font-medium text-xs'>
+                                <p>john doe</p>
+                                <p>1 month ago</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='relative w-full group dark:text-white text-white'>
+                    <div className='z-10 absolute inset-0 bg-gradient-to-t from-black/70 to-transparent bg-blend-overlay rounded-xl'></div>
+                    <div className='relative rounded-xl overflow-hidden w-full h-full'>
+                        <Image src={swiperImageOne} alt='swiper image one' fill className='object-cover object-center rounded-xl group-hover:scale-110 transition duration-500' priority />
+                    </div>
+                    <div className='z-20 absolute rounded-xl top-0 left-0 pt-12 px-8 flex flex-col gap-y-4 md:gap-y-6 md:w-3/4 w-full transition duration-500 group-hover:-translate-y-3'>
+                        <span className='uppercase text-xs font-semibold'>gadget</span>
+                        <h2 className='text-3xl md:text-4xl font-bold leading-normal md:leading-normal'>
+                            <Link href="/blog/blog-one" className='bg-gradient-to-r from-white 
+                to-white bg-[length:0px_1px]
+                hover:bg-[length:100%_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 cursor-pointer'>
+                                Will Humans Be Able to Live in Mars in the Future?
+                            </Link>
+                        </h2>
+                        <div className='flex gap-x-2 items-center'>
+                            <Image src={author} alt='author' className='rounded-full' />
+                            <div className='uppercase font-medium text-xs'>
+                                <p>john doe</p>
+                                <p>1 month ago</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='relative w-full group dark:text-white text-white'>
+                    <div className='z-10 absolute inset-0 bg-gradient-to-t from-black/70 to-transparent bg-blend-overlay rounded-xl'></div>
+                    <div className='relative rounded-xl overflow-hidden w-full h-full'>
+                        <Image src={swiperImageTwo} alt='swiper image two' fill className='object-cover object-center rounded-xl group-hover:scale-110 transition duration-500' priority />
+                    </div>
+                    <div className='z-20 absolute rounded-xl top-0 left-0 pt-12 px-8 flex flex-col gap-y-4 md:gap-y-6 md:w-3/4 w-full transition duration-500 group-hover:-translate-y-3'>
+                        <span className='uppercase text-xs font-semibold'>gadget</span>
+                        <h2 className='text-3xl md:text-4xl font-bold leading-normal md:leading-normal'>
+                            <Link href="/blog/blog-one" className='bg-gradient-to-r from-white 
+                to-white bg-[length:0px_1px]
+                hover:bg-[length:100%_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 cursor-pointer'>
+                                Surprising Benefits of Honeydew Melon
+                            </Link>
+                        </h2>
+                        <div className='flex gap-x-2 items-center'>
+                            <Image src={author} alt='author' className='rounded-full' />
+                            <div className='uppercase font-medium text-xs'>
+                                <p>john doe</p>
+                                <p>1 month ago</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='relative w-full group dark:text-white text-white'>
+                    <div className='z-10 absolute inset-0 bg-gradient-to-t from-black/70 to-transparent bg-blend-overlay rounded-xl'></div>
+                    <div className='relative rounded-xl overflow-hidden w-full h-full'>
+                        <Image src={swiperImageThree} alt='swiper image three' fill className='object-cover object-center rounded-xl group-hover:scale-110 transition duration-500' priority />
+                    </div>
+                    <div className='z-20 absolute rounded-xl top-0 left-0 pt-12 px-8 flex flex-col gap-y-4 md:gap-y-6 md:w-3/4 w-full transition duration-500 group-hover:-translate-y-3'>
+                        <span className='uppercase text-xs font-semibold'>gadget</span>
+                        <h2 className='text-3xl md:text-4xl font-bold leading-normal md:leading-normal'>
+                            <Link href="/blog/blog-one" className='bg-gradient-to-r from-white 
+                to-white bg-[length:0px_1px]
+                hover:bg-[length:100%_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 cursor-pointer'>
+                                Spicy Crispy Chicken Burger Recipe
+                            </Link>
+                        </h2>
+                        <div className='flex gap-x-2 items-center'>
+                            <Image src={author} alt='author' className='rounded-full' />
+                            <div className='uppercase font-medium text-xs'>
+                                <p>john doe</p>
+                                <p>1 month ago</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='relative w-full group dark:text-white text-white'>
+                    <div className='z-10 absolute inset-0 bg-gradient-to-t from-black/70 to-transparent bg-blend-overlay rounded-xl'></div>
+                    <div className='relative rounded-xl overflow-hidden w-full h-full'>
+                        <Image src={swiperImageFour} alt='swiper image four' fill className='object-cover object-center rounded-xl group-hover:scale-110 transition duration-500' priority />
+                    </div>
+                    <div className='z-20 absolute rounded-xl top-0 left-0 pt-12 px-8 flex flex-col gap-y-4 md:gap-y-6 md:w-3/4 w-full transition duration-500 group-hover:-translate-y-3'>
+                        <span className='uppercase text-xs font-semibold'>gadget</span>
+                        <h2 className='text-3xl md:text-4xl font-bold leading-normal md:leading-normal'>
+                            <Link href="/blog/blog-one" className='bg-gradient-to-r from-white 
+                to-white bg-[length:0px_1px]
+                hover:bg-[length:100%_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 cursor-pointer'>
+                                Will Humans Be Able to Live in Mars in the Future?
                             </Link>
                         </h2>
                         <div className='flex gap-x-2 items-center'>
@@ -168,7 +261,8 @@ const SwiperCards = (props: Props) => {
                     modules={[Autoplay, Controller]}
                     autoplay={{
                         delay: 5000,
-                        disableOnInteraction: false
+                        disableOnInteraction: false,
+                        waitForTransition: false
                     }}
                     loop={true}
                     spaceBetween={20}
@@ -185,6 +279,38 @@ const SwiperCards = (props: Props) => {
                     slideToClickedSlide={true}
                     className='md:h-[282px] w-[282px] md:w-auto'
                 >
+                    <SwiperSlide className='swiper-controller'>
+                        <div className="relative w-[72px] h-[72px]">
+                            <Image src={swiperImageOne} alt='swiper one' fill className='object-cover object-center rounded-full border-[2px] border-neutral-300/30' />
+                            <svg viewBox="0 0 72 72" className='controller'>
+                                <circle cx="36" cy="36" r="35"></circle>
+                            </svg>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-controller'>
+                        <div className="relative w-[72px] h-[72px]">
+                            <Image src={swiperImageTwo} alt='swiper one' fill className='object-cover object-center rounded-full border-[2px] border-neutral-300/30' />
+                            <svg viewBox="0 0 72 72" className='controller'>
+                                <circle cx="36" cy="36" r="35"></circle>
+                            </svg>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-controller'>
+                        <div className="relative w-[72px] h-[72px]">
+                            <Image src={swiperImageThree} alt='swiper one' fill className='object-cover object-center rounded-full border-[2px] border-neutral-300/30' />
+                            <svg viewBox="0 0 72 72" className='controller'>
+                                <circle cx="36" cy="36" r="35"></circle>
+                            </svg>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-controller'>
+                        <div className="relative w-[72px] h-[72px]">
+                            <Image src={swiperImageFour} alt='swiper one' fill className='object-cover object-center rounded-full border-[2px] border-neutral-300/30' />
+                            <svg viewBox="0 0 72 72" className='controller'>
+                                <circle cx="36" cy="36" r="35"></circle>
+                            </svg>
+                        </div>
+                    </SwiperSlide>
                     <SwiperSlide className='swiper-controller'>
                         <div className="relative w-[72px] h-[72px]">
                             <Image src={swiperImageOne} alt='swiper one' fill className='object-cover object-center rounded-full border-[2px] border-neutral-300/30' />
