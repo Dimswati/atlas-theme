f16403e8400a3c7071381e90ae6f89bd1d5b149a
Mr Paul <124022001+Dimswati@users.noreply.github.com>
2023-11-11 16:38:48 +0000

    theme icon

--- components/app/ThemeSwitcher.tsx
+++ components/app/ThemeSwitcher.tsx
@@ -11,7 +11,7 @@ const ThemeSwitcher = (props: Props) => {
     const { theme, setTheme } = useTheme()
 
     return (
-        <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="bg-blue-700 text-white hover:bg-blue-700 hover:text-white h-11 rounded-xl">
             {theme === 'dark' ? <BsMoonFill /> : <BsFillSunFill />}
         </Button>
     )
