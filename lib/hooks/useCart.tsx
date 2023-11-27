64e94fac43c807e7d87b8b07137bbe1514bd4420
Spotyne <143184840+Spotyne@users.noreply.github.com>
2023-11-27 14:42:53 +0000

    change cart state

--- lib/hooks/useCart.tsx
+++ lib/hooks/useCart.tsx
@@ -7,7 +7,7 @@ interface CartStore {
 }
 
 const useCart = create<CartStore>((set) => ({
-    isOpen: true,
+    isOpen: false,
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false })
 }))
